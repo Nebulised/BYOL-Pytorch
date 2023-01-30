25d1b9f4dda2ad8ca4cda60417962d2fda23a58d
Nebulised <dhenleymarshall@gmail.com>
2023-01-30 13:31:41 +0000

    Added EMA Added method for updating EMA tau Removed existing built in augmentation methods Split online and target network up for increased clarity Simplified MLP implementation to make it BYOL specific

--- networks.py
+++ networks.py
@@ -1,4 +1,5 @@
 import copy
+import math
 import os
 
 import torch.nn
@@ -6,7 +7,7 @@ import torchvision.transforms
 from torchvision.models import get_model
 from augmentations import *
 from torchvision.transforms import RandomApply, RandomResizedCrop, RandomHorizontalFlip, GaussianBlur, \
-    RandomSolarize, ToTensor, Normalize
+    RandomSolarize, ToTensor, Normalize, InterpolationMode
 
 ACTIVATION_OPTIONS = {
     "relu":torch.nn.ReLU,
@@ -18,7 +19,7 @@ ACTIVATION_OPTIONS = {
 class BYOL(torch.nn.Module):
 
     def __init__(self,
-                 augmentation_params: dict,
+                 max_num_steps,
                  encoder_model: str = "resnet50",
                  embedding_size: int = 128,
                  num_projection_layers: int = 1,
@@ -30,18 +31,20 @@ class BYOL(torch.nn.Module):
                  ema_tau : int = 0.996,
                  name : str = "byol_model"):
         super().__init__()
-        self.aug_params = augmentation_params
         self.name = name
         #TODO: Implement tau updating
+        self.base_tau = ema_tau
         self.current_tau = ema_tau
+        self.current_step = 0
+        self.max_num_steps = max_num_steps
         self.embedding_size = embedding_size
         self.projection_size = projection_size
         self.input_height = input_height
         self.input_width = input_width
 
         self.online_encoder = get_model(name = encoder_model,
-                                        weights = None,
-                                        num_classes = self.embedding_size)
+                                        weights = None)
+        self.online_encoder.fc = torch.nn.Identity()
 
         self.online_projection_head = MLP(num_hidden_layers = num_projection_layers,
                                           size_in = self.embedding_size,
@@ -55,90 +58,84 @@ class BYOL(torch.nn.Module):
                                     hidden_layer_size = projection_hidden_layer_size,
                                     batch_norm_enabled = True)
 
-        self.target_encoder = copy.deepcopy(self.online_encoder)
-        self.target_projection_head = copy.deepcopy(self.online_projection_head)
+        self.target_encoder, self.target_projection_head = self.create_target_network()
 
-        self.online_network = torch.nn.Sequential(self.online_encoder,
-                                                  self.online_projection_head,
-                                                  self.online_predictor)
-        self.target_network = torch.nn.Sequential(self.target_encoder,
-                                                  self.target_projection_head)
+    def create_target_network(self):
+        target_encoder = copy.deepcopy(self.online_encoder)
+        target_projection_head = copy.deepcopy(self.online_projection_head)
 
-        for param in self.target_network.parameters():
+        for param in list(target_encoder.parameters()) + list(target_projection_head.parameters()):
             param.requires_grad = False
+        return target_encoder, target_projection_head
 
-        self.view_1_augs, self.view_2_augs = self.get_augmentations_compositions()
 
+    def update_tau(self):
+        assert self.max_num_steps is not None, "For EMA the max number of training steps must be specified"
+        self.current_tau = 1 - (1-self.base_tau) * (math.cos((math.pi * self.current_step)/self.max_num_steps) + 1)/2
+
+    def set_max_num_steps(self, new_max_num_steps):
+        self.max_num_steps = new_max_num_steps
 
     def update_target_network(self):
+        self.current_step += 1
+        self.update_tau()
         with torch.no_grad():
-            online_network_state_dict = self.online_network.state_dict()
-            target_network_state_dict = self.target_network.state_dict()
-            for name, param in self.target_network.named_parameters():
-                target_network_state_dict[name] = (self.current_tau * param) + ((1-self.current_tau)*online_network_state_dict[name])
+            for online_encoder_param, target_encoder_param in zip(self.online_encoder.parameters(), self.target_encoder.parameters()):
+                target_encoder_param.data = target_encoder_param.data * self.current_tau + (1-self.current_tau) * online_encoder_param.data
+            for online_projector_param, target_projector_param in zip(self.online_projection_head.parameters(), self.target_projection_head.parameters()):
+                target_projector_param.data = target_projector_param.data * self.current_tau + (1-self.current_tau) * online_projector_param.data
+
 
     def save(self, folder_path, epoch, optimiser):
-        torch.save({'epoch':epoch,
-                    'online_network_state_dict':self.online_network.state_dict(),
-                    "target_network_state_dict": self.target_network.state_dict(),
-                    'optimizer_state_dict':optimiser.state_dict(),
+        torch.save({"epoch":epoch,
+                    "online_encoder_state_dict" : self.online_encoder.state_dict(),
+                    "online_projection_head_state_dict" : self.online_projection_head.state_dict(),
+                    "online_predictor_state_dict" : self.online_predictor.state_dict(),
+                    "target_encoder_state_dict" : self.target_encoder.state_dict(),
+                    "target_projection_head_state_dict" : self.target_projection_head.state_dict(),
+                    "optimiser_state_dict" : optimiser.state_dict(),
+                    "current_step" : self.current_step
             }, os.path.join(folder_path, f"{self.name}_epoch={epoch}.pt"))
 
     def load(self, model_path):
         checkpoint = torch.load(model_path)
-        self.online_network.load_state_dict(checkpoint["online_network_state_dict"])
-        self.target_network.load_state_dict(checkpoint["target_network_state_dict"])
-        return checkpoint["optimizer_state_dict"]
-    #TODO: Change optimizer to optimiser
+        self.online_encoder.load_state_dict(checkpoint["online_encoder_state_dict"])
+        self.online_projection_head.load_state_dict(checkpoint["online_projection_head_state_dict"])
+        self.online_predictor.load_state_dict(checkpoint["online_predictor_state_dict"])
+        self.target_encoder.load_state_dict(checkpoint["target_encoder_state_dict"])
+        self.target_projection_head.load_state_dict(checkpoint["target_projection_head_state_dict"])
+        self.current_step = checkpoint["current_step"]
+        return checkpoint["optimiser_state_dict"]
 
-    def get_augmentations_compositions(self):
-        augmentations = []
-        for view in self.aug_params.values():
-            c_j_params = view["colour_jitter"]
-            gauss_blur_params = view["gaussian_blur"]
-            solarization_params = view["solarization"]
-            view_augmentations = torchvision.transforms.Compose([RandomApply([RandomResizedCrop(size = (self.input_height,self.input_width))],
-                                                        p = view["random_crop"]),
-                                            RandomHorizontalFlip(p = view["random_flip"]),
-                                            RandomApply([BYOLColorJitter(max_brightness = c_j_params["brightness_adjustment"],
-                                                                         max_contrast = c_j_params["contrast_adjustment"],
-                                                                         max_saturation = c_j_params["saturation_adjustment"],
-                                                                         max_hue = c_j_params["hue_adjustment"])],
-                                                        p = c_j_params["probability"]),
-                                            RandomApply([GaussianBlur(kernel_size = gauss_blur_params["kernel_size"],
-                                                                      sigma = gauss_blur_params["std"])],
-                                                                      p = gauss_blur_params["probability"]),
-                                            RandomApply([RandomSolarize(threshold = solarization_params["threshold"],
-                                                                        p = solarization_params["probability"])]),
-                                                                 ToTensor(),
-                                                                 Normalize(mean = 0.5,std=0.5)])
-            augmentations.append(view_augmentations)
-        return augmentations
+
+
+    def get_all_online_params(self):
+        parameters = []
+        for model in (self.online_encoder, self.online_projection_head, self.online_predictor):
+            parameters += list(model.parameters())
+        return parameters
 
 
     @ staticmethod
     def regression_loss(predicted: torch.Tensor,
                         expected: torch.Tensor):
         return 2 - (2 * torch.nn.functional.cosine_similarity(x1 = predicted,
-                                                              x2 = expected))
+                                                              x2 = expected,
+                                                              dim=-1))
 
-    def get_image_views(self,
-                        image):
-        return self.view_1_augs(image), self.view_2_augs(image)
+
+
 
     def forward(self,
                 image_1,
-                image_2,
-                inference=False):
-        online_output_1 = self.online_network(image_1)
-        online_output_2 = self.online_network(image_2)
+                image_2):
+        online_output_1 = self.online_predictor(self.online_projection_head(self.online_encoder(image_1)))
+        online_output_2 = self.online_predictor(self.online_projection_head(self.online_encoder(image_2)))
         with torch.no_grad():
-            target_output_1 = self.target_network(image_1).detach()
-            target_output_2 = self.target_network(image_2).detach()
-
+            target_output_1 = self.target_projection_head(self.target_encoder(image_1))
+            target_output_2 = self.target_projection_head(self.target_encoder(image_2))
 
         total_loss = self.regression_loss(online_output_1,target_output_2.detach()) + self.regression_loss(online_output_2,target_output_1.detach())
-
         return total_loss.mean()
 
 
@@ -155,18 +152,16 @@ class LinearLayer(torch.nn.Module):
         if activation not in ACTIVATION_OPTIONS:
             raise ValueError(f"Expected activation function to be one of {list(ACTIVATION_OPTIONS.keys())}. Received"
                              f" {activation}")
-        self.activation = ACTIVATION_OPTIONS[activation]()
-        self.batch_norm = torch.nn.BatchNorm1d(num_features = size_out) if batch_norm_enabled else torch.nn.Identity()
+        self.activation = torch.nn.ReLU()
+        self.batch_norm = torch.nn.BatchNorm1d(num_features = size_out)
         self.linear = torch.nn.Linear(in_features = size_in,
                                       out_features = size_out,
                                       bias = bias)
+        self.all_layers = torch.nn.Sequential(self.linear, self.batch_norm, self.activation)
 
     def forward(self,
-                input_matrix):
-        output = self.linear(input_matrix)
-        output = self.batch_norm(output)
-        output = self.activation(output)
-        return output
+                input_vector):
+       return self.all_layers(input_vector)
 
 
 class MLP(torch.nn.Module):
@@ -179,16 +174,14 @@ class MLP(torch.nn.Module):
                  batch_norm_enabled: bool):
         super(MLP,
               self).__init__()
-        layers = [LinearLayer(size_in = size_in if layer_index == 0 else hidden_layer_size,
-                              size_out = hidden_layer_size,
-                              activation = "relu",
-                              batch_norm_enabled = batch_norm_enabled) for layer_index in range(num_hidden_layers)]
-        layers.append(LinearLayer(size_in = hidden_layer_size,
-                                  size_out = size_out,
-                                  activation = "none",
-                                  batch_norm_enabled = False))
+        self.full_linear_block = LinearLayer(size_in = size_in,
+                                             size_out = hidden_layer_size,
+                                             activation = "relu",
+                                             batch_norm_enabled = batch_norm_enabled)
+        self.output_layer = torch.nn.Linear(in_features = hidden_layer_size,
+                                            out_features = size_out)
 
-        self.all_layers = torch.nn.Sequential(*layers)
+        self.all_layers = torch.nn.Sequential(self.full_linear_block, self.output_layer)
 
     def forward(self,
                 input_vector):
