7521aa0cbb8e1cc4371b9a53d81f95cce48fa839
Nebulised <dhenleymarshall@gmail.com>
2023-02-01 12:27:09 +0000

    Added docstring

--- utils.py
+++ utils.py
@@ -2,21 +2,43 @@ import mlflow
 import yaml
 
 
-def get_params(path : str):
+def get_params(path: str):
+    """ Loads in yaml file and returns dict of contents
+    Args:
+        path:
+            Full path to yaml file
+
+    Returns:
+        dict : Contents of yaml file
+
+    """
     with open(path,
               "r") as yaml_file:
-        try:
-            return yaml.safe_load(yaml_file)
-        except yaml.YAMLError as yaml_error:
-            print(yaml_error)
+        return yaml.safe_load(yaml_file)
 
 
-def log_param_dicts(param_dict, existing_key = None):
+def log_param_dicts(param_dict : dict,
+                    existing_key=None):
+    """Method for logging mlflow params for dict of dicts
 
+    Recursively logs params for the dicts. Stop case is when no longer anymore nested dicts to recurse through
+    Automatically adds the key as a prefix via the param "existing_key"
+
+    Args:
+        param_dict:
+            An N-level nested dictionairy
+        existing_key:
+            The prefix added onto the key for the params
+
+    Returns:
+        None
+    """
     for key, val in param_dict.items():
         current_concat_key = f"{existing_key}_{key}" if existing_key is not None else key
         if type(val) is dict:
-            log_param_dicts(val, existing_key = current_concat_key)
+            log_param_dicts(val,
+                            existing_key=current_concat_key)
         else:
             if "every" not in current_concat_key:
-                mlflow.log_param(current_concat_key, val)
+                mlflow.log_param(current_concat_key,
+                                 val)
