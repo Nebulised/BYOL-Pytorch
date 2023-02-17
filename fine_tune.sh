c8c97e06761eb0bab5650f4633bcfe145d18a010
Nebulised <dhenleymarshall@gmail.com>
2023-02-17 22:26:40 +0000

    Fixed weight decay not being divided by learning rate as per the BYOL paper

--- fine_tune.sh
+++ fine_tune.sh
@@ -8,7 +8,7 @@ MODEL_PATH=""
 for lr in 0.1 0.03162278 0.01 0.00316228 0.001 0.00031623 0.0001; do
   bash update_yaml_param.sh "$FINE_TUNE_PARAM_FILE" "optimiser_params-lr" "$lr"
   for weight_decay in 0.000010 0.000022 0.000046 0.000100 0.000215 0.000464 0.001000 0; do;
-    bash update_yaml_param.sh "$FINE_TUNE_PARAM_FILE" "optimiser_params-weight_decay" "$weight_decay"
+    bash update_yaml_param.sh "$FINE_TUNE_PARAM_FILE" "optimiser_params-weight_decay" "$((weight_decay/lr))"
     python main.py --model-output-folder-path "$OUTPUT_FOLDER_PATH" --dataset-type "cifar10" --dataset-path "$PATH_TO_DATASET" --run-type "fine-tune" --gpu 0 --model-path "$MODEL_PATH" --num-workers 6 --run-para-file-path "$FINE_TUNE_PARAM_FILE" --model-param-file-path "$MODEL_PARAM_FILE"
 
   done
