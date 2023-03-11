0812e5b0a0699e0be3aa151e0bd5b4fc1ec31bbe
Nebulised <dhenleymarshall@gmail.com>
2023-03-11 16:01:08 +0000

    No longer uses random state when creating train and val sets

--- dataset.py
+++ dataset.py
@@ -88,8 +88,7 @@ def get_dataset(type : str,
         train_split_indexes, val_split_indexes = sklearn.model_selection.train_test_split(torch.arange(len(train_dataset)),
                                                                                           train_size=percent_train_to_use_as_val,
                                                                                           test_size=1-percent_train_to_use_as_val,
-                                                                                          stratify=[train_dataset.dataset.targets[index] for index in train_dataset.indices],
-                                                                                          random_state=42)
+                                                                                          stratify=[train_dataset.dataset.targets[index] for index in train_dataset.indices])
 
         new_train_dataset = torch.utils.data.Subset(train_dataset,
                                                     train_split_indexes)
